f919535e1eb39b78f77c3c2b8ccee9244fd7fc92
ahuston-0 <aliceghuston@gmail.com>
2024-08-04 10:32:51 -0400

    add partition and bash_wrapper
    Signed-off-by: ahuston-0 <aliceghuston@gmail.com>

--- rad_development_python.py
+++ rad_development_python.py
@@ -1,5 +1,6 @@
 """common."""
 
+import itertools
 import logging
 import sys
 
@@ -16,3 +17,29 @@ def configure_logger(level: str = "INFO") -> None:
         format="%(asctime)s %(levelname)s %(filename)s:%(lineno)d - %(message)s",
         handlers=[logging.StreamHandler(sys.stdout)],
     )
+
+def partition(predicate, iterable):
+    """Partition entries into false entries and true entries.
+
+    If *predicate* is slow, consider wrapping it with functools.lru_cache().
+    """
+    # partition(is_odd, range(10)) → 0 2 4 6 8   and  1 3 5 7 9
+    t1, t2 = itertools.tee(iterable)
+    return itertools.filterfalse(predicate, t1), filter(predicate, t2)
+
+def bash_wrapper(command: str, path: str = ".") -> tuple[str, str, int]:
+    """Execute a bash command and capture the output.
+
+    Args:
+        command (str): The bash command to be executed.
+        path (str): The current working directory, '.' by default
+
+    Returns:
+        Tuple[str, int]: A tuple containing the output of the command (stdout) as a string,
+        the error output (stderr) as a string (optional), and the return code as an integer.
+    """
+    # This is a acceptable risk
+    process = Popen(command.split(), stdout=PIPE, stderr=PIPE, cwd=path)  # noqa: S603
+    output, error = process.communicate()
+
+    return output.decode(), error.decode(), process.returncode
